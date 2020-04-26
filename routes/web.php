6a9634afb829ff3b0c5850e4391a9b012bd5d5d3
jixiaoxiao <jixiaoxiao0301@163.com>
2020-04-26 17:24:11 +0800

    vue-bootstrap-templates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+   return 'Hello World';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
